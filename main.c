dc7162e338798b1ca5281856d641718455a78a17
oomuro <youkosama@naver.com>
2018-04-07 04:16:00 +0900

    fixed bug

--- main.c
+++ main.c
@@ -25,14 +25,22 @@ int main() {
     }
     printf("\n\n");
 
+    // 테스트용 임시배열 선언
+    char tmp[SIZE];
+
+    for (int i = 0; i < SIZE; i++) {
+        tmp[i] = char_temp[i];
+    }
 
     // 임시 줄바꿈 배열 선언
     int num_temp[] = {0};
+
     int count = 0;
 
     // "\n" 찾아 할당
+    // 버그의 온상
     for (int i = 0; i < SIZE; i++) {
-        if (char_temp[i] == '\n') {
+        if (tmp[i] == '\n') {
             num_temp[count] = i;
             count += 1;
         }
@@ -57,45 +65,27 @@ int main() {
     // 마지막 문자 번호 부여
     int last_index = newline_number[count - 1];
 
-    // 문자 배열 선언
-    char text[last_index];
-
-    // 문자 배열에 텍스트 할당
-    for (int i = 0; i < last_index; i++) {
-        text[i] = char_temp[i];
-    }
-
-    // 문자 확인
-    for (int i = 0; i < SIZE; i++) {
-        printf("%c", text[i]);
-    }
-    printf("\n\n");
-
-
     // 마지막 문자 번호 확인
     printf("%d\n", last_index);
 
     // 문자 배열 선언
-//    char text[count][last_index];
+    char text[count][last_index];
 
     // 문자 배열에 텍스트 할당
-//    for (int j = 0; j < newline_number[0]; j++) {
-//        text[0][j] = char_temp[j];
-//    }
-//
-//    for (int i = 0; i < count; i++) {
-//        for (int j = newline_number[i] + 1; j < newline_number[i + 1]; j++) {
-//            text[i + 1][j] = char_temp[j];
-//        }
-//    }
+    for (int j = 0; j < newline_number[0]; j++) {
+        text[0][j] = char_temp[j];
+    }
 
-//    for (int i = 0; i < count; i++) {
-//        for (int j = 0; j < SIZE; j++) {
-//            printf("%c", text[i][j]);
-//        }
-//    }
+    for (int i = 0; i < count-1; i++) {
+        for (int j = newline_number[i] + 1; j < newline_number[i + 1]; j++) {
+            text[i + 1][j] = char_temp[j];
+        }
+    }
 
-//    printf("%c\n", text[0][1]);
+    // 문자 배열 확인용
+    for (int i = 0; i < SIZE; i++) {
+        printf("%c", text[0][i]);
+    }
 
     return 0;
 }
